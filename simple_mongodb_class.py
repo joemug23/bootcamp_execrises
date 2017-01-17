2bf7e679e35a4180e60592d18dc539a1d8569af4
Jonathan Mugula Zihalirwa <Jonathan Mugula Zihalirwa>
2017-01-17 11:42:51 +0300

    implementing the delete method

--- simple_mongodb_class.py
+++ simple_mongodb_class.py
@@ -17,4 +17,12 @@ class Database(object):
         Database.DBase[collection].insert(data)
         return "Data inserted Successfully"
 
-    
+    @staticmethod
+    def delete(collection, data):
+        # delete a row of a given data
+        results = Database.DBase[collection].find(data)
+        if results:
+            Database.DBase[collection].remove(data)
+            return "Data deleted!"
+        else:
+            return "No record found"
