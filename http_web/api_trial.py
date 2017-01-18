644c85609bf5e6383a794b9378c5956c5a5329b9
Jonathan Mugula Zihalirwa <Jonathan Mugula Zihalirwa>
2017-01-18 15:55:26 +0300

    Add a function to get a github user

--- http_web/api_trial.py
+++ http_web/api_trial.py
@@ -1,10 +1,22 @@
 
 import requests
-# get data from github api using this request.
-# change the user and pass string in the auth tuple with the desired credentials
-req = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-print(req.text)
 
 
+def get_a_user(username, password):
 
+    # get data from github api using this request.
+    # change the user and pass string in the auth tuple with the desired credentials
 
+    req = requests.get('https://api.github.com/user', auth=(username, password))
+    if req.status_code == 200:
+        print(req.text)
+        return req.text
+    elif req.status_code == 404:
+        print(req.status_code)
+        return "User not found"
+    elif req.status_code == 401:
+        print(req.status_code)
+        return "Wrong details, Access denied!"
+
+# call this function with github credentials
+# get_a_user("username", "password")
